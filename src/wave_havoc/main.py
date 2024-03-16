f0eb2429c1d0c365b7b2dcb88d7cf12a7c34b025
Koen Leijsten <koenleijsten@gmail.com>
2024-03-16 10:32:49 +0100

    created a new logic to parse data

--- src/wave_havoc/main.py
+++ src/wave_havoc/main.py
@@ -1,10 +1,8 @@
 import os
 import re
 from spark import Spark
-from typing import List, Iterator
 from datetime import datetime
-from pyspark import RDD
-from pyspark.sql import SparkSession
+from typing import List
 from pyspark.sql.types import (
     StructType,
     StructField,
@@ -15,69 +13,18 @@ from pyspark.sql.types import (
 
 
 def get_data_file_paths(directory_path: str) -> List[str]:
-    """List all files in a directory and return them as a list.
+    """List all files in a directory and return their paths.
 
     Args:
-        directory_path (str): The path to the directory.
+        directory_path (str): The path to the directory containing data files.
 
     Returns:
-        List[str]: A list of filenames in the directory.
+        List[str]: A list of file paths.
     """
-    data_file_paths = []
-    data_files_names = os.listdir(directory_path)
-    for file_name in data_files_names:
-        data_file_paths.append(f"{directory_path}/{file_name}")
-    return data_file_paths
-
-
-def filter_lines(
-    iter, comment_pattern: re.Pattern, header_pattern: re.Pattern
-) -> Iterator[str]:
-    """
-    Filter function to yield only the first line that matches the header pattern and data lines.
-
-    Args:
-        iter (iterable): Iterator over lines.
-        header_pattern (re.Pattern): Regular expression pattern for matching header lines.
-
-    Yields:
-        str: Lines that match the header pattern.
-    """
-    header_yielded = False
-    for line in iter:
-        if header_pattern.match(line) and not header_yielded:
-            header_yielded = True
-            yield line
-        elif not comment_pattern.match(line):
-            yield line
-
-
-def create_rdd(data_file_paths: List[str], spark: SparkSession) -> RDD:
-    """
-    Create one big RDD by joining the content of multiple data files.
-
-    Args:
-        data_file_paths (List[str]): A list of file paths.
-        spark (SparkSession): The SparkSession object.
-
-    Returns:
-        RDD: One big RDD containing the content from all files.
-    """
-    # Define a regex pattern to match lines starting with #
-    comment_pattern = re.compile(r"^#.*$")
-    header_pattern = re.compile(r"#\s*DTG.*LOCATION")
-
-    # Read all files and create one big RDD
-    all_files_rdd = spark.sparkContext.wholeTextFiles(
-        ",".join(data_file_paths)
-    ).values()
-
-    # Apply the filtering function to remove comment lines and yield only the first header line
-    filtered_rdd = all_files_rdd.flatMap(lambda text: text.split("\n")).mapPartitions(
-        lambda iter: filter_lines(iter, comment_pattern, header_pattern)
-    )
-
-    return filtered_rdd
+    return [
+        os.path.join(directory_path, file_name)
+        for file_name in os.listdir(directory_path)
+    ]
 
 
 def parse_line(line: str, col_starts: List[int]) -> List[str]:
@@ -88,16 +35,12 @@ def parse_line(line: str, col_starts: List[int]) -> List[str]:
         col_starts (List[int]): A list of column start positions.
 
     Returns:
-        List: A list of parsed values.
+        List[str]: A list of parsed values.
     """
-    if len(col_starts) > 1:
-        values = [
-            line[start:end].strip() if end else line[start:].strip()
-            for start, end in zip(col_starts, col_starts[1:])
-        ]
-    else:
-        values = [line[start:].strip() for start in col_starts]
-    return values
+    return [
+        line[start:end].strip() if end else line[start:].strip()
+        for start, end in zip(col_starts, col_starts[1:])
+    ]
 
 
 def convert_line_values(values: List[str], schema: StructType) -> List:
@@ -110,87 +53,75 @@ def convert_line_values(values: List[str], schema: StructType) -> List:
     Returns:
         List: A list of converted values.
     """
-    casted_values = []
+    converted_values = []
     for i, value in enumerate(values):
         field_type = schema.fields[i].dataType
         if field_type == TimestampType():
-            casted_values.append(
+            converted_values.append(
                 datetime.strptime(value, "%Y-%m-%d %H:%M:%S") if value != "" else None
             )
         elif field_type == DoubleType():
-            casted_values.append(float(value) if value != "" else None)
+            converted_values.append(float(value) if value != "" else None)
         else:
-            casted_values.append(value)
-    return casted_values
+            converted_values.append(value)
+    return converted_values
 
 
-############################## MAIN ##############################
+if __name__ == "__main__":
+    # Get raw data file locations
+    directory_path = os.path.join(os.getcwd(), "data", "raw")
+    data_file_paths = get_data_file_paths(directory_path)
 
-data_file_paths = get_data_file_paths(directory_path=f"{os.getcwd()}/data/raw/")
+    # Get spark session
+    spark = Spark.set_master("local[*]").get_session()
 
-spark = Spark.set_master("local[*]").get_session()
+    # Create RDD from text files
+    all_files_rdd = spark.sparkContext.textFile(",".join(data_file_paths))
 
-rdd = create_rdd(data_file_paths=data_file_paths[:3], spark=spark)
+    # Filter header lines
+    header_pattern = re.compile(r"#\s*DTG.*LOCATION")
+    header_line = all_files_rdd.filter(lambda line: header_pattern.match(line)).first()
 
-# Define the schema
-schema = StructType(
-    [
-        StructField("DTG", TimestampType(), True),
-        StructField("LOCATION", StringType(), True),
-        StructField("NAME", StringType(), True),
-        StructField("LATITUDE", DoubleType(), True),
-        StructField("LONGITUDE", DoubleType(), True),
-        StructField("ALTITUDE", DoubleType(), True),
-        StructField("U_BOOL_10", StringType(), True),
-        StructField("T_DRYB_10", DoubleType(), True),
-        StructField("TN_10CM_PAST_6H_10", DoubleType(), True),
-        StructField("T_DEWP_10", DoubleType(), True),
-        StructField("T_DEWP_SEA_10", DoubleType(), True),
-        StructField("T_DRYB_SEA_10", DoubleType(), True),
-        StructField("TN_DRYB_10", DoubleType(), True),
-        StructField("T_WETB_10", DoubleType(), True),
-        StructField("TX_DRYB_10", DoubleType(), True),
-        StructField("U_10", DoubleType(), True),
-        StructField("U_SEA_10", DoubleType(), True),
+    # Get column starts of header_line
+    col_starts = [0] + [
+        match.end() for match in re.finditer(r"\s{2,}", header_line.lstrip("#"))
     ]
-)
 
-header_line = rdd.take(1)[0]
+    # Set Schema of DataFrame
+    schema = StructType(
+        [
+            StructField("DTG", TimestampType(), True),
+            StructField("LOCATION", StringType(), True),
+            StructField("NAME", StringType(), True),
+            StructField("LATITUDE", DoubleType(), True),
+            StructField("LONGITUDE", DoubleType(), True),
+            StructField("ALTITUDE", DoubleType(), True),
+            StructField("U_BOOL_10", StringType(), True),
+            StructField("T_DRYB_10", DoubleType(), True),
+            StructField("TN_10CM_PAST_6H_10", DoubleType(), True),
+            StructField("T_DEWP_10", DoubleType(), True),
+            StructField("T_DEWP_SEA_10", DoubleType(), True),
+            StructField("T_DRYB_SEA_10", DoubleType(), True),
+            StructField("TN_DRYB_10", DoubleType(), True),
+            StructField("T_WETB_10", DoubleType(), True),
+            StructField("TX_DRYB_10", DoubleType(), True),
+            StructField("U_10", DoubleType(), True),
+            StructField("U_SEA_10", DoubleType(), True),
+        ]
+    )
 
-# Calculate the start position of each column
-col_starts = [0] + [
-    match.end() for match in re.finditer(r"\s{2,}", header_line.lstrip("#"))
-]
+    # Skip header line, filter comment lines, and parse data
+    data_rdd = (
+        all_files_rdd.filter(lambda line: not line.startswith("#"))
+        .map(lambda line: parse_line(line, col_starts))
+        .map(lambda values: convert_line_values(values, schema))
+    )
 
-# Skip the header line and then apply the converter function to each line
-data = (
-    rdd.zipWithIndex()
-    .filter(lambda x: x[1] > 0)
-    .map(lambda x: parse_line(x[0], col_starts))
-    .map(lambda values: convert_line_values(values, schema))
-)
+    # Create DataFrame
+    df = spark.createDataFrame(data_rdd, schema=schema)
 
-# Convert RDD to DataFrame with specified schema
-df = spark.createDataFrame(data, schema=schema)
+    # Filter on De Bilt
+    df_filtered = df.filter(df["LOCATION"] == "260_T_a")
+    df_filtered.show()
 
-# Filter on De Bilt
-df = df.filter(df["LOCATION"] == "260_T_a")
-
-df.show()
-
-# Stop SparkSession
-spark.stop()
-
-############################## Stappen ##############################
-
-# 1. Eerst berekenen van gewenste output
-# 2. test voor alle bestanden
-# 3. Output wegschrijven Dataframe, daarna pas berekenen
-
-# Setup correctly
-# Write tests
-# get request
-# Run in Docker
-# Include coldwaves
-
-# write logging code
+    spark.stop()
