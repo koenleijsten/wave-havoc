bdde97251bcd9c116dc406aa0dcf7633b531cde8
Koen Leijsten <koenleijsten@gmail.com>
2024-03-17 12:07:19 +0100

    working application

--- src/wave_havoc/wave_havoc.py
+++ src/wave_havoc/wave_havoc.py
@@ -1,5 +1,9 @@
 import os
 import re
+import time
+from pyspark.sql import functions as F
+from dotenv import load_dotenv
+from pyspark.sql.window import Window
 from spark import Spark
 from get_data import get_data, extract_data, remove_existing_data
 from parse_data import (
@@ -16,9 +20,19 @@ from pyspark.sql.types import (
     TimestampType,
 )
 
-if __name__ == "__main__":
+
+def main():
     # Set main directory
     main_dir = f"{os.path.dirname(os.path.realpath(__file__))}/../.."
+
+    # Load ENV variables
+    load_dotenv()
+    NUM_WORKERS = os.getenv("NUM_WORKERS")
+    LOCATION = os.getenv("LOCATION")
+    THRESHOLD_TEMP = os.getenv("THRESHOLD_TEMP")
+    TROPICAL_DAY_TEMP = os.getenv("TROPICAL_DAY_TEMP")
+    DURATION_IN_DAYS_THRESHOLD = os.getenv("DURATION_IN_DAYS_THRESHOLD")
+    NUMBER_OF_TROPICAL_DAYS_THRESHOLD = os.getenv("NUMBER_OF_TROPICAL_DAYS_THRESHOLD")
 
     # Remove existing data if it exists
     # remove_existing_data(output_dir=main_dir)
@@ -36,7 +50,7 @@ if __name__ == "__main__":
     spark = Spark.set_master("local[*]").get_session()
 
     # Create RDD from text files
-    rdd = spark.sparkContext.textFile(",".join(data_file_paths))
+    rdd = spark.sparkContext.textFile(",".join(data_file_paths[:2]))
 
     # Filter first header line
     header_pattern = re.compile(r"#\s*DTG.*LOCATION")
@@ -76,23 +90,113 @@ if __name__ == "__main__":
     )
 
     # Create DataFrame
-    df = spark.createDataFrame(data, schema=schema)
+    df_init = spark.createDataFrame(data, schema=schema)
 
-    # Filter on De Bilt
-    df_filtered = df.filter(df["LOCATION"] == "260_T_a")
-    df_filtered.show()
+    # Select relevant Columns and filter on "De Bilt"
+    df_filtered = df_init.select("DTG", "LOCATION", "TX_DRYB_10").filter(
+        df_init["LOCATION"] == LOCATION
+    )
 
+    # Calculate max temperature per date
+    df_grouped = df_filtered.groupBy(
+        F.date_format(F.col("DTG"), "yyyy-MM-dd").alias("date"),
+        F.col("LOCATION").alias("location"),
+    ).agg(F.max("TX_DRYB_10").alias("max_temperature"))
+
+    # Repartition
+    df_grouped = df_grouped.repartition(numPartitions=NUM_WORKERS)
+
+    # Order by date ASC
+    df_ordered = df_grouped.orderBy(F.asc("date"))
+
+    # Add boolean column for tropical day
+    df_tropical = df_ordered.withColumn(
+        "tropical_day",
+        F.when(F.col("max_temperature") > TROPICAL_DAY_TEMP, 1).otherwise(0),
+    )
+
+    # Add boolean column for day above threshold
+    df_threshold = df_tropical.withColumn(
+        "above_threshold",
+        F.when(F.col("max_temperature") > THRESHOLD_TEMP, 1).otherwise(0),
+    )
+
+    # Only select days above threshold
+    df_threshold_filtered = df_threshold.filter(F.col("above_threshold") != 0)
+
+    # Define a window specification for ordering by date
+    window = Window.partitionBy("date").orderBy("date")
+
+    # Use lag function to check the previous date
+    df_with_lag = df_threshold_filtered.withColumn(
+        "prev_date", F.lag("date", 1).over(window)
+    )
+
+    # Add column to calculate the date difference with previous row
+    df_date_diff = df_with_lag.withColumn(
+        "date_diff", F.datediff(df_with_lag["date"], df_with_lag["prev_date"])
+    )
+
+    # Add column to flag each consecutive days run start
+    df_flag = df_date_diff.withColumn(
+        "flag_run",
+        F.when((F.col("date_diff") > 1) | (F.col("date_diff").isNull()), 1).otherwise(
+            0
+        ),
+    )
+
+    # Add column to assign group id's to consecutive day runs
+    df_group_id = df_flag.withColumn("group_id", F.sum("flag_run").over(window))
+
+    # Aggregate to final DataFrame
+    df_result = (
+        df_group_id.groupBy("group_id")
+        .agg(
+            F.min("date").alias("from_date"),
+            F.max("date").alias("to_date"),
+            F.sum("above_threshold").alias("duration_in_days"),
+            F.sum("tropical_day").alias("number_of_tropical_days"),
+            F.max("max_temperature").alias("max_temperature"),
+        )
+        .select(
+            "from_date",
+            "to_date",
+            "duration_in_days",
+            "number_of_tropical_days",
+            "max_temperature",
+        )
+    )
+
+    # Only select heatwaves
+    df_result = df_result.filter(
+        (F.col("duration_in_days") >= DURATION_IN_DAYS_THRESHOLD)
+        & (F.col("number_of_tropical_days") >= NUMBER_OF_TROPICAL_DAYS_THRESHOLD)
+    )
+
+    # Convert output to JSON
+    output = df_result.toJSON().collect()
+
+    # Stop Spark Session
     spark.stop()
+
+    return output
+
+
+if __name__ == "__main__":
+    main()
 
 ############################## Stappen ##############################
 
-# 1. Eerst berekenen van gewenste output
-# 2. test voor alle bestanden (DONE)
 
-# Setup correctly
+# Add error handling
+# Setup correctly (including documentation)
 # Write tests
-# get request (DONE)
-# Run in Docker
-# Include coldwaves
 
+# Run in Docker
+
+# Include coldwaves
 # write logging code
+
+# get request (DONE)
+# 1. Eerst berekenen van gewenste output (DONE)
+# 2. test voor alle bestanden (DONE)
