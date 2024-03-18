6efb4f6d6fc2b5637deeaa0131ecaad7b4b71395
Koen Leijsten <koenleijsten@gmail.com>
2024-03-18 11:07:28 +0100

    added new function to remove archive file

--- src/wave_havoc/get_data.py
+++ src/wave_havoc/get_data.py
@@ -15,7 +15,7 @@ def remove_existing_data(output_dir: str) -> None:
         if os.path.exists(output_dir):
             shutil.rmtree(output_dir)
     except Exception as e:
-        print(f"Error occurred while removing existing data: {e}")
+        raise e
 
 
 def get_data(url: str, output_dir: str) -> None:
@@ -25,14 +25,11 @@ def get_data(url: str, output_dir: str) -> None:
         url (str): The URL to download the data from.
         output_dir (str): The path to the output directory.
     """
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
+    response = requests.get(url)
+    response.raise_for_status()
 
-        with open(os.path.join(output_dir, "data.tgz"), "wb") as f:
-            f.write(response.content)
-    except Exception as e:
-        print(f"Error occurred while getting data: {e}")
+    with open(os.path.join(output_dir, "data.tgz"), "wb") as f:
+        f.write(response.content)
 
 
 def extract_data(output_dir: str, filter: Optional[str] = None) -> None:
@@ -42,8 +39,17 @@ def extract_data(output_dir: str, filter: Optional[str] = None) -> None:
         output_dir (str): The path to the output directory.
         filter (str, optional): Filter to extract specific files or directories.
     """
-    try:
-        with tarfile.open(os.path.join(output_dir, "data.tgz"), "r:gz") as tar:
-            tar.extractall(output_dir, filter=filter)
-    except Exception as e:
-        print(f"Error occurred while extracting data: {e}")
+    with tarfile.open(os.path.join(output_dir, "data.tgz"), "r:gz") as tar:
+        tar.extractall(output_dir, filter=filter)
+
+
+def clean_up_data_archive(output_dir: str) -> None:
+    """Clean up the data archive file after extraction.
+
+    Args:
+        output_dir (str): The path to the output directory.
+    """
+
+    archive_file = os.path.join(output_dir, "data.tgz")
+    if os.path.exists(archive_file):
+        os.remove(archive_file)
--- src/wave_havoc/wave_havoc.py
+++ src/wave_havoc/wave_havoc.py
@@ -4,7 +4,7 @@ from pyspark.sql import functions as F
 from dotenv import load_dotenv
 from pyspark.sql.window import Window
 from spark import Spark
-from get_data import get_data, extract_data, remove_existing_data
+from get_data import get_data, extract_data, remove_existing_data, clean_up_data_archive
 from parse_data import (
     get_data_file_paths,
     get_column_start_positions,
@@ -44,143 +44,146 @@ def main():
     get_data(url=url, output_dir=main_dir)
     extract_data(output_dir=main_dir, filter="data")
 
-    # Get raw data file locations
-    data_file_paths = get_data_file_paths(data_directory)
+    # Remove archive file
+    clean_up_data_archive(output_dir=main_dir)
 
-    # Get spark session
-    spark = Spark.set_master("local[*]").get_session()
+    # # Get raw data file locations
+    # data_file_paths = get_data_file_paths(data_directory)
 
-    # Create RDD from text files
-    rdd = spark.sparkContext.textFile(",".join(data_file_paths))
+    # # Get spark session
+    # spark = Spark.set_master("local[*]").get_session()
 
-    # Filter first header line
-    header_pattern = re.compile(r"#\s*DTG.*LOCATION")
-    header_line = rdd.filter(lambda line: header_pattern.match(line)).first()
+    # # Create RDD from text files
+    # rdd = spark.sparkContext.textFile(",".join(data_file_paths))
 
-    # Get column starts from header_line
-    col_starts = get_column_start_positions(header_line)
+    # # Filter first header line
+    # header_pattern = re.compile(r"#\s*DTG.*LOCATION")
+    # header_line = rdd.filter(lambda line: header_pattern.match(line)).first()
 
-    # Set Schema of DataFrame
-    schema = StructType(
-        [
-            StructField("DTG", TimestampType(), True),
-            StructField("LOCATION", StringType(), True),
-            StructField("NAME", StringType(), True),
-            StructField("LATITUDE", DoubleType(), True),
-            StructField("LONGITUDE", DoubleType(), True),
-            StructField("ALTITUDE", DoubleType(), True),
-            StructField("U_BOOL_10", StringType(), True),
-            StructField("T_DRYB_10", DoubleType(), True),
-            StructField("TN_10CM_PAST_6H_10", DoubleType(), True),
-            StructField("T_DEWP_10", DoubleType(), True),
-            StructField("T_DEWP_SEA_10", DoubleType(), True),
-            StructField("T_DRYB_SEA_10", DoubleType(), True),
-            StructField("TN_DRYB_10", DoubleType(), True),
-            StructField("T_WETB_10", DoubleType(), True),
-            StructField("TX_DRYB_10", DoubleType(), True),
-            StructField("U_10", DoubleType(), True),
-            StructField("U_SEA_10", DoubleType(), True),
-        ]
-    )
+    # # Get column starts from header_line
+    # col_starts = get_column_start_positions(header_line)
 
-    # Filter comment lines, and parse data
-    data = (
-        rdd.filter(lambda line: not line.startswith("#"))
-        .map(lambda line: parse_line(line, col_starts))
-        .map(lambda values: convert_line_values(values, schema))
-    )
+    # # Set Schema of DataFrame
+    # schema = StructType(
+    #     [
+    #         StructField("DTG", TimestampType(), True),
+    #         StructField("LOCATION", StringType(), True),
+    #         StructField("NAME", StringType(), True),
+    #         StructField("LATITUDE", DoubleType(), True),
+    #         StructField("LONGITUDE", DoubleType(), True),
+    #         StructField("ALTITUDE", DoubleType(), True),
+    #         StructField("U_BOOL_10", StringType(), True),
+    #         StructField("T_DRYB_10", DoubleType(), True),
+    #         StructField("TN_10CM_PAST_6H_10", DoubleType(), True),
+    #         StructField("T_DEWP_10", DoubleType(), True),
+    #         StructField("T_DEWP_SEA_10", DoubleType(), True),
+    #         StructField("T_DRYB_SEA_10", DoubleType(), True),
+    #         StructField("TN_DRYB_10", DoubleType(), True),
+    #         StructField("T_WETB_10", DoubleType(), True),
+    #         StructField("TX_DRYB_10", DoubleType(), True),
+    #         StructField("U_10", DoubleType(), True),
+    #         StructField("U_SEA_10", DoubleType(), True),
+    #     ]
+    # )
 
-    # Create DataFrame
-    df_init = spark.createDataFrame(data, schema=schema)
+    # # Filter comment lines, and parse data
+    # data = (
+    #     rdd.filter(lambda line: not line.startswith("#"))
+    #     .map(lambda line: parse_line(line, col_starts))
+    #     .map(lambda values: convert_line_values(values, schema))
+    # )
 
-    # Select relevant Columns and filter on "De Bilt"
-    df_filtered = df_init.select("DTG", "LOCATION", "TX_DRYB_10").filter(
-        df_init["LOCATION"] == LOCATION
-    )
+    # # Create DataFrame
+    # df_init = spark.createDataFrame(data, schema=schema)
 
-    # Calculate max temperature per date
-    df_grouped = df_filtered.groupBy(
-        F.date_format(F.col("DTG"), "yyyy-MM-dd").alias("date"),
-        F.col("LOCATION").alias("location"),
-    ).agg(F.max("TX_DRYB_10").alias("max_temperature"))
+    # # Select relevant Columns and filter on "De Bilt"
+    # df_filtered = df_init.select("DTG", "LOCATION", "TX_DRYB_10").filter(
+    #     df_init["LOCATION"] == LOCATION
+    # )
 
-    # Repartition
-    df_grouped = df_grouped.repartition(numPartitions=NUM_WORKERS)
+    # # Calculate max temperature per date
+    # df_grouped = df_filtered.groupBy(
+    #     F.date_format(F.col("DTG"), "yyyy-MM-dd").alias("date"),
+    #     F.col("LOCATION").alias("location"),
+    # ).agg(F.max("TX_DRYB_10").alias("max_temperature"))
 
-    # Order by date ASC
-    df_ordered = df_grouped.orderBy(F.asc("date"))
+    # # Repartition
+    # df_grouped = df_grouped.repartition(numPartitions=NUM_WORKERS)
 
-    # Add boolean column for tropical day
-    df_tropical = df_ordered.withColumn(
-        "tropical_day",
-        F.when(F.col("max_temperature") > TROPICAL_DAY_TEMP, 1).otherwise(0),
-    )
+    # # Order by date ASC
+    # df_ordered = df_grouped.orderBy(F.asc("date"))
 
-    # Add boolean column for day above threshold
-    df_threshold = df_tropical.withColumn(
-        "above_threshold",
-        F.when(F.col("max_temperature") > THRESHOLD_TEMP, 1).otherwise(0),
-    )
+    # # Add boolean column for tropical day
+    # df_tropical = df_ordered.withColumn(
+    #     "tropical_day",
+    #     F.when(F.col("max_temperature") > TROPICAL_DAY_TEMP, 1).otherwise(0),
+    # )
 
-    # Only select days above threshold
-    df_threshold_filtered = df_threshold.filter(F.col("above_threshold") != 0)
+    # # Add boolean column for day above threshold
+    # df_threshold = df_tropical.withColumn(
+    #     "above_threshold",
+    #     F.when(F.col("max_temperature") > THRESHOLD_TEMP, 1).otherwise(0),
+    # )
 
-    # Define a window specification for ordering by date
-    window = Window.partitionBy("location").orderBy("date")
+    # # Only select days above threshold
+    # df_threshold_filtered = df_threshold.filter(F.col("above_threshold") != 0)
 
-    # Use lag function to check the previous date
-    df_with_lag = df_threshold_filtered.withColumn(
-        "prev_date", F.lag("date", 1).over(window)
-    )
+    # # Define a window specification for ordering by date
+    # window = Window.partitionBy("location").orderBy("date")
 
-    # Add column to calculate the date difference with previous row
-    df_date_diff = df_with_lag.withColumn(
-        "date_diff", F.datediff(df_with_lag["date"], df_with_lag["prev_date"])
-    )
+    # # Use lag function to check the previous date
+    # df_with_lag = df_threshold_filtered.withColumn(
+    #     "prev_date", F.lag("date", 1).over(window)
+    # )
 
-    # Add column to flag each consecutive days run start
-    df_flag = df_date_diff.withColumn(
-        "flag_run",
-        F.when((F.col("date_diff") > 1) | (F.col("date_diff").isNull()), 1).otherwise(
-            0
-        ),
-    )
+    # # Add column to calculate the date difference with previous row
+    # df_date_diff = df_with_lag.withColumn(
+    #     "date_diff", F.datediff(df_with_lag["date"], df_with_lag["prev_date"])
+    # )
 
-    # Add column to assign group id's to consecutive day runs
-    df_group_id = df_flag.withColumn("group_id", F.sum("flag_run").over(window))
+    # # Add column to flag each consecutive days run start
+    # df_flag = df_date_diff.withColumn(
+    #     "flag_run",
+    #     F.when((F.col("date_diff") > 1) | (F.col("date_diff").isNull()), 1).otherwise(
+    #         0
+    #     ),
+    # )
 
-    # Aggregate to final DataFrame
-    df_result = (
-        df_group_id.groupBy("group_id")
-        .agg(
-            F.min("date").alias("from_date"),
-            F.max("date").alias("to_date"),
-            F.sum("above_threshold").alias("duration_in_days"),
-            F.sum("tropical_day").alias("number_of_tropical_days"),
-            F.max("max_temperature").alias("max_temperature"),
-        )
-        .select(
-            "from_date",
-            "to_date",
-            "duration_in_days",
-            "number_of_tropical_days",
-            "max_temperature",
-        )
-    )
+    # # Add column to assign group id's to consecutive day runs
+    # df_group_id = df_flag.withColumn("group_id", F.sum("flag_run").over(window))
 
-    # Only select heatwaves
-    df_result = df_result.filter(
-        (F.col("duration_in_days") >= DURATION_IN_DAYS_THRESHOLD)
-        & (F.col("number_of_tropical_days") >= NUMBER_OF_TROPICAL_DAYS_THRESHOLD)
-    )
+    # # Aggregate to final DataFrame
+    # df_result = (
+    #     df_group_id.groupBy("group_id")
+    #     .agg(
+    #         F.min("date").alias("from_date"),
+    #         F.max("date").alias("to_date"),
+    #         F.sum("above_threshold").alias("duration_in_days"),
+    #         F.sum("tropical_day").alias("number_of_tropical_days"),
+    #         F.max("max_temperature").alias("max_temperature"),
+    #     )
+    #     .select(
+    #         "from_date",
+    #         "to_date",
+    #         "duration_in_days",
+    #         "number_of_tropical_days",
+    #         "max_temperature",
+    #     )
+    # )
 
-    # Convert output to JSON
-    output = df_result.toJSON().collect()
+    # # Only select heatwaves
+    # df_result = df_result.filter(
+    #     (F.col("duration_in_days") >= DURATION_IN_DAYS_THRESHOLD)
+    #     & (F.col("number_of_tropical_days") >= NUMBER_OF_TROPICAL_DAYS_THRESHOLD)
+    # )
 
-    # Stop Spark Session
-    spark.stop()
+    # # Convert output to JSON
+    # output = df_result.toJSON().collect()
 
-    return output
+    # # Stop Spark Session
+    # spark.stop()
+
+    # return output
 
 
 if __name__ == "__main__":
